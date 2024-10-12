e3ca5de02e22515bc4b9dbed5d182d56c390930f
Shivani <shivanijedia92@mail.com>
2024-10-12 15:14:00 +0530

    add all file

---
+++ src/Oct/ex_02102024MainMethod/Lab001.java
@@ -0,0 +1,9 @@
+package Oct.ex_02102024MainMethod;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("https://github.com/ShivaniJedia/ATB9xJavaPrograms/settings");
+        System.out.println("https://github.com/ShivaniJedia/ATB9xJavaPrograms.git");
+    }
+}
